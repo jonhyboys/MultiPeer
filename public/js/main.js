bd130e48cd098626343b2a3781baa44b97dec891
Jonhy Olivas <jonhyboys@gmail.com>
2018-08-31 08:58:25 -0600

    Cambio de diseño, Inicio terminado

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,5 @@
 'use strict';
-
 (function() {
-    //Variables globales
     var glob_nombre_sala = '';
     var glob_nombre_usuario = '';
     var glob_error = 0;
@@ -15,115 +13,94 @@
         glob_local_stream,
         glob_es_iniciador = false,
         socket;
-
     $(document).ready(function() {
-        //Iniciar los sockets
         socket = io();
-
-        //Poner el foco al nombre de la sala
-        $('#txt-nombre-sala').focus();
-
-        //Click en botón conectar
+        $('#txt-nombre-sala, #txt-nombre-usuario').focus(enfocar_caja_texto);
+        $('#txt-nombre-sala, #txt-nombre-usuario').blur(desenfocar_caja_texto);
         $('#btn-conectar').click(function(evt) {
             evt.preventDefault();
-            //Ocultar los mensajes de error
-            $('#msj-error-sala, #msj-error-usuario, #msj-sala-llena').addClass('hide');
-            $('#txt-nombre-sala, #txt-nombre-usuario').removeClass('is-invalid');
-            //Obtener nombre de sala y de usuario
+            $('#msj-sala-llena').addClass('hide');
+            //$('#txt-nombre-sala, #txt-nombre-usuario').removeClass('is-invalid');
             glob_nombre_sala = $('#txt-nombre-sala').val();
             glob_nombre_usuario = $('#txt-nombre-usuario').val();
-            //Si el nombre de sala es vacío, mostrar error
-            if (glob_nombre_sala == '') {
-                $('#msj-error-sala').removeClass('hide');
-                $('#txt-nombre-sala').addClass('is-invalid');
+            if (glob_nombre_sala == '' || $('#txt-nombre-sala').hasClass('error')) {
+                $('#txt-nombre-sala').val('Escriba el nombre de la sala').addClass('error');
+                $('#lbl-nombre-sala').addClass('error');
                 glob_error++;
             }
-            //Si el nombre de usuario es vacío, mostrar error
-            if (glob_nombre_usuario == '') {
-                $('#msj-error-usuario').removeClass('hide');
-                $('#txt-nombre-usuario').addClass('is-invalid');
+            if (glob_nombre_usuario == '' || $('#txt-nombre-usuario').hasClass('error')) {
+                $('#txt-nombre-usuario').val('Escriba el nombre de usuario').addClass('error');
+                $('#lbl-nombre-usuario').addClass('error')
                 glob_error++;
             }
-            //No continuar si hay un error
             if (glob_error > 0) {
                 glob_error = 0;
                 return;
             }
-            //Establecer nombre de sala y usuario en la barra superior
-            $('#li-nombre-sala').html(glob_nombre_sala);
-            $('#li-nombre-usuario').html(glob_nombre_usuario);
-            //Unirse o crear(si no existe) la sala
             socket.emit('crear_unir_sala', {
                 nombre_sala: glob_nombre_sala,
                 nombre_usuario: glob_nombre_usuario
             });
         });
-
-        //Enviar chat escrito
         $('#btn-enviar').click(function() {
-            //Obtener el texto escrito
             var _mensaje = $('#txt-mensaje-enviar').val();
-            //Si el texto es diferente de vacío enviar mensaje
             if (_mensaje != '') {
-                //Pegar mi mj en la lista de mensajes
                 $('#lista-mensajes').append('<li><strong>' + glob_nombre_usuario + ':</strong> ' + _mensaje + '</li>');
-                //Objeto a enviar, se envía el usuario para que el remitente sepa de quién es el mensaje
                 var obj_msg = {
                     nombre_sala: glob_nombre_sala,
                     nombre_usuario: glob_nombre_usuario,
                     mensaje: _mensaje
                 };
                 socket.emit('mensaje', obj_msg);
-                //Se limpia la caja de texto
                 $('#txt-mensaje-enviar').val('');
             }
         });
-
-        //Mostrar lista de usuarios conectados
-        $('#lbl-usuarios-conectados').click(function() { if ($('#lista-usuarios-conectados').html() != '') { $('#lista-usuarios-conectados').toggleClass('visible'); } });
-
-        //Apagar/Encender audio o video
-        $('#btn-video-control').click(function() { glob_local_stream.getVideoTracks()[0].enabled = !(glob_local_stream.getVideoTracks()[0].enabled); });
-        $('#btn-audio-control').click(function() { glob_local_stream.getAudioTracks()[0].enabled = !(glob_local_stream.getAudioTracks()[0].enabled); });
-
+        $('#lbl-usuarios-conectados').click(function() {
+            if ($('#lista-usuarios-conectados').html() != '')
+                $('#lista-usuarios-conectados').toggleClass('visible');
+        });
+        $('#btn-video-control').click(function() {
+            glob_local_stream.getVideoTracks()[0].enabled = !(glob_local_stream.getVideoTracks()[0].enabled);
+        });
+        $('#btn-audio-control').click(function() {
+            glob_local_stream.getAudioTracks()[0].enabled = !(glob_local_stream.getAudioTracks()[0].enabled);
+        });
         $('#btn-colgar').click(function() { location.reload(); });
-
-        //Cuando el servidor indica que se creó la sala, pasamos a la siguiente vista
         socket.on('sala_creada', function(datos) {
             $('#inicio').toggleClass('hide');
             $('#principal').toggleClass('hide');
+            $('#li-nombre-sala').html(glob_nombre_sala);
+            $('#li-nombre-usuario').html(glob_nombre_usuario);
             glob_es_iniciador = true;
             mi_socket_id = datos.id;
             pc_iniciador = new conexion(0, false);
         });
-
-        //Cuando el servidor indica que se nos agregó a la sala, pasamos a la siguiente vista
         socket.on('agregado_a_sala', function(datos) {
             $('#inicio').toggleClass('hide');
             $('#principal').toggleClass('hide');
+            $('#li-nombre-sala').html(glob_nombre_sala);
+            $('#li-nombre-usuario').html(glob_nombre_usuario);
             mi_socket_id = datos.id;
             var x;
             for (x = 0; x < datos.ids_conectados.length; x++) {
-                $('#lista-usuarios-conectados').append('<li data-nombre="' + datos.usuarios_conectados[x] + '">' + datos.usuarios_conectados[x] + '</li>');
+                $('#lista-usuarios-conectados').
+                append('<li data-nombre="' + datos.usuarios_conectados[x] + '">' + datos.usuarios_conectados[x] + '</li>');
                 $('<video id="' + datos.ids_conectados[x] + '" autoplay src=""></video>').insertBefore('#contenedor-botones-video');
                 pcs[datos.ids_conectados[x]] = new conexion(datos.ids_conectados[x], true);
             }
         });
-
-        //Cuando la sala está llena, se le indica al usuario
-        socket.on('sala_llena', function() { $('#msj-sala-llena').removeClass('hide'); });
-
-        //Verificar el nombre de usuario
-        socket.on('usuario_existe', function() { $('#msj-usuario-existe').removeClass('hide'); });
-
-        //Pegar mensajes recibidos de otros usuarios en la lista de mensajes
-        socket.on('mensaje', function(datos) { $('#lista-mensajes').append('<li><strong>' + datos.nombre_usuario + ':</strong> ' + datos.mensaje + '</li>'); });
-
-        //Agregar nuevos usuarios conectados
+        socket.on('sala_llena', function() {
+            $('#msj-sala-llena').removeClass('hide');
+        });
+        socket.on('usuario_existe', function() {
+            $('#msj-usuario-existe').removeClass('hide');
+        });
+        socket.on('mensaje', function(datos) {
+            $('#lista-mensajes').
+            append('<li><strong>' + datos.nombre_usuario + ':</strong> ' + datos.mensaje + '</li>');
+        });
         socket.on('usuario_agregado', function(datos) {
-            //Agregar usuario nuevo a la lista de usuarios conectados
             $('#lista-usuarios-conectados').append('<li data-nombre=' + datos.nuevo_usuario + '>' + datos.nuevo_usuario + '</li>');
-            //Crear conexión con el nuevo usuario
             $('<video id="' + datos.nuevo_id + '" autoplay src=""></video>').insertBefore('#contenedor-botones-video');
             if (glob_es_iniciador && pc_iniciador != undefined) {
                 glob_es_iniciador = false;
@@ -138,7 +115,6 @@
                 pcs[datos.nuevo_id] = nueva_conexion;
             }
         });
-
         socket.on('descripcion', function(datos) {
             if (datos.descripcion.type == 'offer') {
                 console.log('Oferta de: ' + datos.socket_origen);
@@ -152,21 +128,18 @@
                 console.log('Respuesta de: ' + datos.socket_origen);
             }
         });
-
         socket.on('candidato', function(datos) {
             console.log('Candidato recibido de: ' + datos.socket_origen);
             pcs[datos.socket_origen].cliente.addIceCandidate(datos.candidato);
         });
-
         socket.on('desconectar', function(datos) {
             if (pcs[datos.id] != undefined) {
                 delete pcs[datos.id];
                 $('#' + datos.id).remove();
                 $('#lista-usuarios-conectados li[data-nombre="' + datos.nombre + '"]').remove();
-                establecer_tamano_chat();
+                //establecer_tamano_chat();
             }
         });
-
         window.addEventListener('beforeunload', function() {
             socket.emit('desconectar', {
                 nombre_sala: glob_nombre_sala,
@@ -176,13 +149,30 @@
         }, false);
     });
 
+    function enfocar_caja_texto() {
+        var input_error = $(this).hasClass('error');
+        var input_inicial = ($(this).val() == '');
+        if (input_inicial || (!input_inicial && input_error)) {
+            $(this).addClass('editing').removeClass('error').val('');
+            $(this).prev().addClass('editing').removeClass('error');
+        }
+    }
+
+    function desenfocar_caja_texto() {
+        var input_inicial = ($(this).val() == '');
+        if (input_inicial) {
+            $(this).removeClass('editing');
+            $(this).prev().removeClass('editing');
+        }
+    }
+
     function establecer_tamano_chat() {
         var ancho_pantalla = $(document).width();
         if (ancho_pantalla >= 414) {
             var video_tamano = $('#contenedor-videos').height();
-            $('#contenedor-chat').removeClass('hide');
             $('#lista-mensajes').height(video_tamano - 67);
         }
+        $('#contenedor-chat').removeClass('hide');
     }
 
     function mostrar_video_local(objeto, ofertar, stream) {
@@ -190,14 +180,11 @@
         stream.getAudioTracks()[0].enabled = false;
         video_local.srcObject = stream;
         glob_local_stream = stream;
-        video_local.onloadeddata = establecer_tamano_chat;
+        //video_local.onloadeddata = establecer_tamano_chat;
         if (!glob_es_iniciador) {
             pcs[objeto].cliente.addStream(stream);
-            //Cuando hayan candidatos enviarlo a otros usuarios
             pcs[objeto].cliente.onicecandidate = enviar_candidato.bind(null, pcs[objeto]);
-            //Una vez que se tenga la descripción local, crear y enviar la oferta a otros usuarios
             if (ofertar) { pcs[objeto].cliente.onnegotiationneeded = crear_oferta.bind(null, pcs[objeto]); }
-            // once remote track arrives, show it in the remote video element
             pcs[objeto].cliente.ontrack = mostrar_video_remoto.bind(null, pcs[objeto]);
         }
     }
@@ -205,8 +192,7 @@
     function conexion(destino, ofertar) {
         this.socket_id_destino = destino;
         this.cliente = new RTCPeerConnection(pcConfig);
-
-        navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+        navigator.mediaDevices.getUserMedia({ audio: true, video: { width: { ideal: 640 }, height: { ideal: 480 } } })
             .then(mostrar_video_local.bind(null, this.socket_id_destino, ofertar))
             .catch(mostrar_error);
     }
@@ -214,7 +200,7 @@
     function mostrar_video_remoto(objeto, evt) {
         var video_remoto = document.getElementById(objeto.socket_id_destino);
         video_remoto.srcObject = evt.streams[0];
-        video_remoto.onloadeddata = establecer_tamano_chat;
+        //video_remoto.onloadeddata = establecer_tamano_chat;
     }
 
     function enviar_candidato(objeto, evt) {
@@ -249,8 +235,11 @@
         });
     }
 
-    function mostrar_error(error) { console.log(error.name + ': ' + error.message); }
+    function mostrar_error(error) {
+        console.log(error.name + ': ' + error.message);
+    }
 
-    function crear_respuesta(objeto, evt) { return objeto.cliente.createAnswer(); }
-
+    function crear_respuesta(objeto, evt) {
+        return objeto.cliente.createAnswer();
+    }
 })();
--- server.js
+++ server.js
@@ -1,54 +1,39 @@
-//  OpenShift sample Node application
+/* VARIABLES - SERVIDOR WEB */
 var express = require('express');
 var app = express();
-var morgan = require('morgan');
 var server = require('http').Server(app);
+var port = process.env.PORT || 8080,
+    ip = process.env.IP || '0.0.0.0';
+
+/* VARIABLES - SERVIDOR WEBSOCKET */
 var io = require('socket.io')(server);
+var _usuarios_conectados = new Array(),
+    _ids_conectados = new Array();
 
-Object.assign = require('object-assign');
-
+/* CONFIGURACIÓN DEL SERVIDOR WEB */
 app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'));
 app.use(express.static('public'));
-
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-
-app.get('/', function(req, res) {
-    // try to initialize the db on every request if it's not already
-    // initialized.
-    res.render('index.html');
-});
-
-// error handling
+app.get('/', function(req, res) { res.render('index.html'); });
 app.use(function(err, req, res, next) {
     console.error(err.stack);
     res.status(500).send('Something bad happened!');
 });
-
+/* INICIO DEL SERVIDOR WEB */
 server.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
 
-var _usuarios_conectados = new Array(),
-    _ids_conectados = new Array();
-
+/*INICIO DEL SERVIDOR WEBSOCKET */
 io.on('connection', function(socket) {
-    socket.on('disconnect', function() {
-        console.log('Se desconectó un usuario.\n');
+    socket.on('mensaje', function(datos) { 
+        socket.to(datos.nombre_sala).emit('mensaje', datos); 
     });
 
-    socket.on('mensaje', function(datos) {
-        socket.to(datos.nombre_sala).emit('mensaje', datos);
-    });
-
-    //Verificar si existe la sala indicada por el usuario
     socket.on('crear_unir_sala', function(datos) {
         var usuarios_en_la_sala = io.sockets.adapter.rooms[datos.nombre_sala];
         var numero_usuarios = usuarios_en_la_sala ? Object.keys(usuarios_en_la_sala.sockets).length : 0;
-
         if (numero_usuarios == 0) {
-            socket.join(datos.nombre_sala); //Crear la sala
-            socket.emit('sala_creada', { id: socket.id }); //Avisar al usuario que ha creado la sala
+            socket.join(datos.nombre_sala);
+            socket.emit('sala_creada', { id: socket.id });
             _ids_conectados[datos.nombre_sala] = [socket.id];
             _usuarios_conectados[datos.nombre_sala] = [datos.nombre_usuario];
         } else if (numero_usuarios < 4) {
@@ -65,20 +50,16 @@ io.on('connection', function(socket) {
                     nuevo_usuario: datos.nombre_usuario,
                     nuevo_id: socket.id
                 });
-            } else {
-                socket.emit('usuario_existe');
-            }
-        } else {
-            socket.emit('sala_llena');
-        }
+            } else { socket.emit('usuario_existe'); }
+        } else { socket.emit('sala_llena'); }
     });
 
-    socket.on('candidato', function(datos) {
-        io.to(datos.socket_destino).emit('candidato', datos);
+    socket.on('candidato', function(datos) { 
+        io.to(datos.socket_destino).emit('candidato', datos); 
     });
 
-    socket.on('descripcion', function(datos) {
-        io.to(datos.socket_destino).emit('descripcion', datos);
+    socket.on('descripcion', function(datos) { 
+        io.to(datos.socket_destino).emit('descripcion', datos); 
     });
 
     socket.on('desconectar', function(datos) {
